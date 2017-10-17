3f6fe43b3e8a178945c7bb2e0cfd5263c26b6994
Spazdude2 <spaziani96@gmail.com>
2017-10-17 11:40:05 -0400

    Start of FizzBuzz

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            String[] FizzBuzz = new string[100];
+
+            for (int i = 0; i <= FizzBuzz.Length; i++)
+            {
+                if (num % 3 == 0 && num % 5 != 0)
+                {
+                    Console.Write("Fizz. ");
+                }
+
+                if (num % 5 == 0 && num % 3 != 0)
+                {
+                    Console.Write("Buzz. ");
+                }
+
+                else
+                {
+                    Console.Write(num + ". ");
+                }
+                num++;
+            }
+        }
+    }
+}
