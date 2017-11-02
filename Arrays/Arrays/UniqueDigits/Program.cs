b00c5bf9e2f43dfe46eb47694623100d691eba54
yasmin johnson <yasminj2019@gmail.com>
2017-11-02 12:45:45 -0400

    unique digits

--- Arrays/Arrays/UniqueDigits/Program.cs
+++ Arrays/Arrays/UniqueDigits/Program.cs
@@ -11,7 +11,49 @@ namespace UniqueDigits
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[50];
+            Random rand = new Random();
+            bool unique = true;
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 50);   
+            }
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    unique = true;
+                    if (arr[i] == arr[j] && arr[j] <= 50)
+                    {
+                        arr[j] = arr.Length + 1;
+                        unique = false;
+                    }
+                }
+                
+
+                /*for (int j = i - 1; j >= 0; j--)
+                {
+                    unique = true;
+                    if (arr[i] == arr[j] && arr[j] <= 50)
+                    {
+                        arr[j] = arr.Length + 1;
+                        unique = false;
+                    }
+                }*/
+
+                if (unique == false)
+                {
+                    arr[i] = arr.Length + 1;
+                }
+            }
+            foreach (int index in arr)
+            {
+                if (index <= arr.Length)
+                {
+                    Console.WriteLine(index);
+                }
+            }
         }
     }
 }
